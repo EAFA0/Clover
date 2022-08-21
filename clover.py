b424b6ae7af083de75ef6aaa5d2d13912e48fd18
EAFA0 <TobiichiOrigami.d@gmail.com>
2022-08-21 15:58:08 +0800

    更新获取 openid 的方式

--- clover.py
+++ clover.py
@@ -15,20 +15,37 @@ def ping():
 
 @app.route("/openid", methods=["GET"])
 def openid():
-    if code := request.args.get("code", ""):
-        url = "https://api.weixin.qq.com/sns/jscode2session"
-        data = {
-            "appid": config.AppID,
-            "secret": config.AppSecret,
-            "js_code": code,
-            "grant_type": "authorization_code",
-        }
-        if (resp := get(url, params=data)) and resp.status_code == 200 and resp.text:
-            if (body := json.loads(resp.text)) and "openid" in body:
-                return {"openid": body["openid"]}
-            else:
-                return body["errmsg"] if "errmsg" in body else "error"
+    if val := openid(request):
+        return {"openid": val}
+    else:
+        return {"msg": "failed"}
 
-    return {
-        "msg": "failed"
-    }, 502
+
+def openid(req: request) -> str:
+    '''
+    获取用户的 openid
+    '''
+    # 云托管服务请求头自带 openid
+    if "x-wx-openid" in req.headers:
+        return req.headers["x-wx-openid"]
+
+    # 非托管服务需要在请求中带上 js_code 字段
+    js_code = request.args.get("js_code", "")
+    if not js_code:
+        raise Exception("empty js_code")
+
+    url = "https://api.weixin.qq.com/sns/jscode2session"
+    data = {
+        "appid": config.AppID,
+        "secret": config.AppSecret,
+        "js_code": js_code,
+        "grant_type": "authorization_code",
+    }
+    if not (resp := get(url, params=data)):
+        raise Exception("request failed")
+
+    if resp.status_code == 200 and resp.text:
+        body = json.loads(resp.text)
+        if "openid" not in body:
+            raise Exception("empty openid response")
+        return body["openid"]
